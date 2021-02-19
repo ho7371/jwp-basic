5edc21c0ef485457f1d10a84e395c3c10822a52b
zino <zino@zino-PC>
2021-02-20 00:32:01 +0900

    로그인/로그아웃 기능

---
+++ src/main/java/next/web/UserLoginServlet.java
@@ -0,0 +1,36 @@
+package next.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import core.db.DataBase;
+import next.model.User;
+
+@WebServlet("/user/login")
+public class UserLoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2904915675594473275L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userId = req.getParameter("userId");
+		String password = req.getParameter("password");
+		
+		User user = DataBase.findUserById(userId);
+		
+		if (user == null || !user.getPassword().equals(password)) {
+			resp.sendRedirect("/user/login_failed.html");
+		} else {
+			HttpSession session = req.getSession();
+			session.setAttribute("user", user);
+			
+			resp.sendRedirect("/user/list");
+		}
+	}
+}
